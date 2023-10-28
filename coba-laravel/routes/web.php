186edfe4bde7717e8df4d20b0f122bdb633fd4a6
Kinn-01 <gilmanarief23@gmail.com>
2023-10-28 14:52:22 +0700

    Video Laravel ke 3 : Struktur Folder, Routes & View

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => 'Gilman Arief Firmansyah',
+        "email" => 'Gilmanarief23@gmail.com',
+        "image" => 'panther.jpg'
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
