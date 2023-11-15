fcfa220f9b20888c25273c1e6fe6ea6b35a5b1e4
Kinn-01 <gilmanarief23@gmail.com>
2023-11-15 14:03:13 +0700

    Video Laravel Ke 14 : View Login & Registration

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
 use App\Http\Controllers\PostController;
-use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 use App\Models\Category;
-use App\Models\User;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -48,3 +48,5 @@ Route::get('/categories', function () {
     ]);
 });
 
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::get('/register', [RegisterController::class, 'index']);
