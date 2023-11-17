533fcb70f98fc9de3ab63d3237df998dabbb1061
Kinn-01 <gilmanarief23@gmail.com>
2023-11-17 15:36:43 +0700

    Video Laravel Ke 17 : Dashboard UI

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -1,12 +1,13 @@
 <?php
 
+use App\Http\Controllers\DashboardPostController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 use App\Models\Category;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
-use App\Http\Controllers\DashboardController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -59,4 +60,10 @@ Route::post('/logout', [LoginController::class, 'logout']);
 Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
 Route::post('/register', [RegisterController::class, 'store']);
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth');
+Route::get('/dashboard', function() {
+    return view('dashboard.index');
+})->middleware('auth');
+
+
+
+Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
