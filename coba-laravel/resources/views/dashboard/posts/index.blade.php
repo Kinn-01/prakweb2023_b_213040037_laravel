ebd70d9349c15a5cd737e2e120402e1d1b68d407
Kinn-01 <gilmanarief23@gmail.com>
2023-11-24 21:24:29 +0700

    Video Laravel ke 20 : Update & Delete Post

--- coba-laravel/resources/views/dashboard/posts/index.blade.php
+++ coba-laravel/resources/views/dashboard/posts/index.blade.php
@@ -30,8 +30,15 @@
           <td>{{ $post->category->name }}</td>
           <td>
             <a href="/dashboard/posts/{{ $post->slug }}" class="badge bg-info"><i class="bi bi-eye"></i></a>
-            <a href="" class="badge bg-warning"><i class="bi bi-pencil"></i></a>
-            <a href="" class="badge bg-danger"><i class="bi bi-x-circle"></i></a>
+            <a href="/dashboard/posts/{{ $post->slug }}/edit" class="badge bg-warning"><i class="bi bi-pencil"></i></a>
+
+            <form action="/dashboard/posts/{{ $post->slug }}" method="POST" class="d-inline">
+              @method('delete')
+              @csrf
+              <button class="badge bg-danger border-0" onclick="return confirm('Are You Sure!!')"><i class="bi bi-x-circle"></i></button>
+              <input type="hidden" name="slug" value="{{ $post->slug }}" >
+            
+            </form>
           </td>
         </tr>
         @endforeach
--- coba-laravel/resources/views/dashboard/posts/show.blade.php
+++ coba-laravel/resources/views/dashboard/posts/show.blade.php
@@ -8,8 +8,15 @@
             <h1 class="mb-3">{{ $post->title }}</h1>
            
             <a href="/dashboard/posts" class="btn btn-success"><i class="bi bi-box-arrow-in-left"></i> Back To All My Posts</a>
-            <a href="" class="btn btn-warning"><i class="bi bi-pencil"></i> Edit</a>
-            <a href="" class="btn btn-danger"><i class="bi bi-x-circle"></i></i> Delete</a>
+            <a href="/dashboard/posts/{{ $post->slug }}/edit" class="btn btn-warning"><i class="bi bi-pencil"></i> Edit</a>
+
+            <form action="/dashboard/posts/{{ $post->slug }}" method="POST" class="d-inline">
+                @method('delete')
+                @csrf
+                <button class="btn btn-danger" onclick="return confirm('Are You Sure!!')"><i class="bi bi-x-circle"></i> Delete</button>
+                <input type="hidden" name="slug" value="{{ $post->slug }}" >
+              
+            </form>
 
             <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" alt="{{ $post->category->name }}" class="img-fluid mt-3">
 
